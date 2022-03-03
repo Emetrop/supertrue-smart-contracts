4a2dd04c8d695487137905ff1fbcd070cde87ddc
Roy <toledoroy@gmail.com>
2022-03-03 14:22:30 -0800

    Additional Config Functions

--- contracts/interfaces/IConfig.sol
+++ contracts/interfaces/IConfig.sol
@@ -3,5 +3,10 @@
 pragma solidity ^0.8.0;
 
 interface IConfig {
+    /// Arbitrary contract designation signature
+    function role() external view returns (bytes16);
+    /// Contract Role Signature
     function getTreasuryData() external view returns (address, uint256);
+    /// Check if Address Has Admin Privileges
+    function isAdmin(address account) external view returns (bool);
 }
