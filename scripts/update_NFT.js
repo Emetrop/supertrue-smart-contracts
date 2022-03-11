2bbaa93835daa42b0540b300d81de4c98d2e628e
Roy <toledoroy@gmail.com>
2022-03-10 19:19:21 -0800

    Update update_NFT.js

--- scripts/update_NFT.js
+++ scripts/update_NFT.js
@@ -1,14 +1,14 @@
 const { ethers, upgrades } = require("hardhat");
 
 // const localChainId = "31337";
-const factoryAddress = "0x55544B6A8a3B4FaA22BB5303F3Ebb15ED0344d3F";
+// const factoryAddress = "0x55544B6A8a3B4FaA22BB5303F3Ebb15ED0344d3F";
+const factoryAddress = "0x086C1A95773a1ec76F5D8C7350B5D220cbcc640A";
 
 async function main() {
     const [deployer, admin] = await ethers.getSigners();
 
     //Log
     // console.log("Deployer account:", deployer.address);
-    console.log("Update NFT Beacon:", deployer.address);
 
     //-- Factory
     const ForwardCreator = await ethers.getContractFactory("ForwardCreator");
@@ -28,12 +28,13 @@ async function main() {
     let newImplementation = await NewImplementation.deploy();
     //Validate Upgrade
     await upgrades.prepareUpgrade(BeaconAddress, NewImplementation);
-        
+
     //Upgrade
     factoryContract.upgradeBeacon(newImplementation.address);
 
     //Log
-    console.log("ForwardNFT Beacon Contract Updated");
+    let newBeaconAddress = await factoryContract.beaconAddress();
+    console.log("Updated NFT Beacon Implementation:", newBeaconAddress);    
 }
   
 main()
